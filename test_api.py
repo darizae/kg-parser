40d8c39639675cba7706c0ec6cc87842d28a5ea6
darizae <danielarizae@gmail.com>
2025-02-13 16:53:25 +0900

    Add test API case for using LLaMa3 HF

--- test_api.py
+++ test_api.py
@@ -22,19 +22,41 @@ jan_config = ModelConfig(
     endpoint_url="http://localhost:1337/v1/chat/completions"
 )
 
+llama3_config = ModelConfig(
+    model_type=ModelType.HUGGINGFACE,
+    model_name_or_path="/remote/csifs1/disk3/Data/jiahao_huang/models/models--meta-llama--Meta-Llama-3-70B-Instruct/snapshots/7129260dd854a80eb10ace5f61c20324b472b31c"
+)
+
 texts = [
     "Mount Everest is the highest mountain in the world. It's located in Nepal.",
     "Marie Curie discovered radium and won Nobel Prizes in Physics and Chemistry."
 ]
 
-# Test OpenAI API
-openai_parser = KGParser(openai_config)
-openai_results = openai_parser.parse_batch(texts)
-openai_parser.save_to_json(openai_results, Path("api_openai_output_dict.json"), "dict")
-openai_parser.save_to_json(openai_results, Path("api_openai_output_list.json"), "list")
+model = "llama"
 
-# Test Jan API
-# jan_parser = KGParser(jan_config)
-# jan_results = jan_parser.parse_batch(texts)
-# jan_parser.save_to_json(jan_results, Path("api_jan_output_list.json"), "list")
-# jan_parser.save_to_json(jan_results, Path("api_jan_output_dict.json"), "dict")
+if __name__ == "__main__":
+
+    if model == "openai":
+        # Test OpenAI API
+        openai_parser = KGParser(openai_config)
+        openai_results = openai_parser.parse_batch(texts)
+        openai_parser.save_to_json(openai_results, Path("api_openai_output_dict.json"), "dict")
+        openai_parser.save_to_json(openai_results, Path("api_openai_output_list.json"), "list")
+
+    elif model == "jan":
+        # Test Jan API
+        jan_parser = KGParser(jan_config)
+        jan_results = jan_parser.parse_batch(texts)
+        jan_parser.save_to_json(jan_results, Path("api_jan_output_list.json"), "list")
+        jan_parser.save_to_json(jan_results, Path("api_jan_output_dict.json"), "dict")
+
+    elif model == "llama":
+        # Test LLaMa3 from HuggingFace
+        llama_parser = KGParser(llama3_config)
+        llama_results = llama_parser.parse_batch(texts)
+        llama_parser.save_to_json(llama_results, Path("api_llama_output_list.json"), "list")
+        llama_parser.save_to_json(llama_results, Path("api_llama_output_dict.json"), "dict")
+
+
+
+
